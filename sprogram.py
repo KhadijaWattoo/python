e4de39bd1552362b902531dcaff3d74c1e33b9eb
KhadijaWattoo <khadijawattoo67@gmail.com>
2025-08-12 04:17:26 +0500

    sprogram.py

---
+++ sprogram.py
@@ -0,0 +1,5 @@
+# This program is related about how we use escape sequence in the python 
+# And how it is works?
+print("hey",4+9, sep="*", end="return0\n")
+print(2,6)
+print("I'm a good developer\"Front-end Developer\" \nand my friend is also a good a developer \"Back-end Developer\" \nft full stack developer")
