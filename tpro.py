0ee728b7be614580d9a0a9925179a9fc823d57fa
KhadijaWattoo <khadijawattoo67@gmail.com>
2025-08-13 03:33:43 +0500

    tpro.py

---
+++ tpro.py
@@ -0,0 +1,13 @@
+a = 10.1
+a2=12
+print (a+a2)
+print(a)
+b = True
+print(b)
+c = "kj"
+d = None
+print ("Print the type of a is ",type(a))
+print ("Print the type of b is ",type(b))
+print ("Print the type of c is ",type(c))
+print ("Print the type of d is ",type(d))
+# This program is about variables and data type in Python.
