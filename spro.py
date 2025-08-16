84256d9e6cdb08c251e6a6951527a4cd4175b797
KhadijaWattoo <khadijawattoo67@gmail.com>
2025-08-16 23:50:11 +0500

    spro.py

---
+++ spro.py
@@ -0,0 +1,11 @@
+# basically this program is about type casting 
+# explicit casting
+a = "1"
+b = "2"
+print("the sum of both values is ",(int(a))+(int(b)))
+# This is an exampleof explicit casting
+
+# implicit casting
+e = 2
+f = 2.5
+print("the sum of both values is ",e+f)
