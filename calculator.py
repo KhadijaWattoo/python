2998709d8009405479fc7ccacb45aa8a506e4f6f
KhadijaWattoo <khadijawattoo67@gmail.com>
2025-08-18 18:47:15 +0500

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Makes a basic or very simple calculator in Python
+a = input("Enter the first number : ")
+b = input("Enter the second number : ")
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+print(int(a) // int(b))
+print(int(a) ** int(b))
